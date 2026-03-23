4fb2fee59e35e2409b2c713a997cf5b17f44f5cc
Alex Martínez <almama08@alumnatflorida.es>
2026-03-23 20:40:34 +0100

    editar

--- controller/Controller.php
+++ controller/Controller.php
@@ -46,6 +46,30 @@ class Controller{
         exit;
     }
 
-    
+    public function editar(){
+        if($_SERVER['REQUEST_METHOD']=="POST"){
+            $tipo = $_POST['tipo_vehiculo'];
+        
+        if ($tipo=="Coche") {
+            $vehiculo= new Coche($_POST['marca'],$_POST['modelo'],$_POST['matricula'],$_POST['precio_dia'],
+            $_POST['numero_puertas'],$_POST['tipo_combustible']);
+        } else {
+            if(isset($_POST['incluye_casco'])){
+                $casco=true;
+            }else{
+                $casco=false;
+            }
+            $vehiculo = new Motocicleta($_POST['marca'],$_POST['modelo'],$_POST['matricula'],
+            $_POST['precio_dia'],$_POST['cilindrada'],$casco);
+        }
+
+        $this->gestor->modificar($vehiculo); 
+        header("Location: index.php");
+        exit;
+        }
+        $matricula=$_GET['matricula'];
+        $vehiculo=$this->gestor->obtenervehiculo($matricula);
+        include 'views/editar.php';
+    }
 }
 ?>
--- models/GestorPDO.php
+++ models/GestorPDO.php
@@ -71,5 +71,50 @@ public function eliminar($matricula){
     $stmt->bindValue(':mat',$matricula);
     return $stmt->execute();
 }
+
+public function obtenerVehiculo($matricula){
+    $sql='SELECT * FROM flota_vehiculos WHERE matricula=:mat';
+    $stmt=$this->getConn()->prepare($sql);
+    $stmt->bindValue(':mat',$matricula);
+    $stmt->execute();
+
+    $res=$stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($res){
+        if($res['tipo_vehiculo']=="Coche"){
+            return new Coche($res['marca'], $res['modelo'], $res['matricula'], $res['precio_dia'], $res['numero_puertas'], $res['tipo_combustible']);
+        }else{
+            return new Motocicleta($res['marca'], $res['modelo'], $res['matricula'], $res['precio_dia'], $res['cilindrada'], $res['incluye_casco']);
+        }
+    }
+    return null;
+}
+
+public function modificar($vehiculo){
+    $sql="UPDATE flota_vehiculos SET marca = :marca, 
+    modelo = :modelo, precio_dia = :precio, 
+    numero_puertas = :puertas, tipo_combustible = :motor, 
+    cilindrada = :cil, incluye_casco = :casco WHERE matricula = :mat";
+
+    $stmt=$this->getConn()->prepare($sql);
+
+    $stmt->bindValue(':marca', $vehiculo->getMarca());
+    $stmt->bindValue(':modelo', $vehiculo->getModelo());
+    $stmt->bindValue(':precio', $vehiculo->getPrecioDia());
+    $stmt->bindValue(':mat', $vehiculo->getMatricula());
+
+    if(get_class($vehiculo)=="Coche"){
+        $stmt->bindValue(':puertas', $vehiculo->getNumeroPuertas());
+        $stmt->bindValue(':motor', $vehiculo->getTipoCombustible());
+        $stmt->bindValue(':cil', null);
+        $stmt->bindValue(':casco', null);
+    }else{
+        $stmt->bindValue(':cil', $vehiculo->getCilindrada());
+        $stmt->bindValue(':casco', $vehiculo->getIncluyeCasco(), PDO::PARAM_BOOL);
+        $stmt->bindValue(':puertas', null);
+        $stmt->bindValue(':motor', null);
+    }
+    return $stmt->execute();
+}
 }
 ?>
